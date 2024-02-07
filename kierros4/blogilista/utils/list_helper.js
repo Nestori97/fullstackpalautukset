ce77e8131e3fde56115417539c892f54ff1e6fb1
Nestori97 <128605318+Nestori97@users.noreply.github.com>
2024-02-07 14:59:14 +0200

    4.4-4.5

--- kierros4/blogilista/utils/list_helper.js
+++ kierros4/blogilista/utils/list_helper.js
@@ -1,7 +1,36 @@
 const dummy = (blogs) => {
     return 1
   }
-  
+  const totalLikes = (blogs) => {
+    if(blogs.length === 0){
+        return 0
+    }
+    else if (blogs.length === 1) 
+    {   
+        return blogs[0].likes
+    }
+    else(blogs.length>0)
+    {  
+        var intilialValue = blogs[0].likes
+        return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes,intilialValue)-intilialValue
+    }
+  }
+  const favoriteBlog = (blogs) => {
+    const getMostLikedBlog = (a,b) =>{
+        var mostLikes =Math.max(a.likes, b.likes)
+        if(mostLikes === a.likes){
+            return a
+        }
+        else return b
+    }
+    if(blogs.length === 0){
+        return []
+    }
+    var mostLiked=blogs.reduce(getMostLikedBlog)
+    return mostLiked
+  }
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
   }
