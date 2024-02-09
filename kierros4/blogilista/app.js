968df7a7602e0dcaad766ef1b3b2612f26180980
Nestori97 <128605318+Nestori97@users.noreply.github.com>
2024-02-09 11:31:49 +0200

    tehtävät 4.15-4.16

--- kierros4/blogilista/app.js
+++ kierros4/blogilista/app.js
@@ -5,10 +5,11 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
-const logger = require('./utils/logger.js')
+const usersRouter = require('./controllers/users')
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users',usersRouter)
 module.exports = app
---
+++ kierros4/blogilista/controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const user = require('../models/user')
+usersRouter.get('/', async(request,response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+usersRouter.post('/', async (request, response) => {
+    const body = request.body
+    const users = await User.find({})
+    const saltRounds = 10
+    if(!body.password || !body.username){
+        return response.status(400).json({error: "username or password missing"})
+    }
+    console.log(body.username)
+    console.log(body.username.length)
+
+    if(body.password.length<3 || body.username.length <3 ){
+        return response.status(400).json({error: "username and password must be atleas 3 characters long"})
+    }
+    const areThereSameNamedUser =users.map(user => user.username === body.username).filter(isittrue => isittrue=== true)
+    if(areThereSameNamedUser.length>0){
+        return response.status(400).json({error: "username must be uniques"})
+    }
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const newUser = new User({
+        username: body.username,
+        passwordHash: passwordHash,
+        name: body.name
+    })
+    const savedUser = await newUser.save()
+    response.status(201).json(savedUser)
+})
+module.exports = usersRouter
