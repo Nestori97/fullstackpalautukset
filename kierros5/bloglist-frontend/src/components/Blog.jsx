e766b044a608f1306d0588969219be9cb96639aa
Nestori97 <128605318+Nestori97@users.noreply.github.com>
2024-07-09 15:16:13 +0300

    5.7 ilman like nappulaa

--- kierros5/bloglist-frontend/src/components/Blog.jsx
+++ kierros5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,20 @@
-const Blog = ({ blog }) => (
+import { useState } from 'react';
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  if(visible)
+  {return(
+  
   <div>
-    {blog.title} {blog.author}
+    {blog.title} <button onClick={toggleVisibility}>hide</button> <br />{blog.author} <br />likes {blog.likes}
   </div>  
-)
-
+)}
+else
+return(
+<div>
+    {blog.title} <button onClick={toggleVisibility}>view</button>
+    </div>  )
+}
 export default Blog
