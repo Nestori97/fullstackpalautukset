8cdce8fae2ea6bd6ae2b8721de9290c40ed95144
Nestori97 <128605318+Nestori97@users.noreply.github.com>
2024-07-12 13:19:10 +0300

    5.14

--- kierros5/bloglist-frontend/src/components/blog.test.jsx
+++ kierros5/bloglist-frontend/src/components/blog.test.jsx
@@ -1,13 +1,35 @@
-import { render, screen } from '@testing-library/react';
-import Blog from './Blog';
-import React from 'react'; // Make sure to import React if you're using JSX
-
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+import React from 'react'
+import userEvent from '@testing-library/user-event'
 test('renders content', () => {
   const blog = {
     title: 'Component testing is done with react-testing-library',
-  };
-  
-  render(<Blog blog={blog} />);
-  const element = screen.getByText('Component testing is done with react-testing-library');
-  expect(element).toBeDefined();
-});
+  }
+
+  render(<Blog blog={blog} />)
+  const element = screen.getByText('Component testing is done with react-testing-library')
+  expect(element).toBeDefined()
+})
+test('view button works correctly', async () => {
+  const blog = {
+    title: 'behind this title is likes, author, url',
+    url: 'www.aalto.fi',
+    likes: 1,
+    author: 'pekka jämsä',
+    user: 'sanna marin',
+  }
+  render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+  screen.debug()
+  const url = screen.getByText((content, element) => element.tagName.toLowerCase() === 'div' && content.includes('www.aalto.fi'))
+  const likes = screen.getByText((content, element) => element.tagName.toLowerCase() === 'div' && content.includes('1'))
+  const userr = screen.getByText((content, element) => element.tagName.toLowerCase() === 'div' && content.includes('pekka jämsä'))
+
+  // Assertions
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
+  expect(userr).toBeDefined()
+})
