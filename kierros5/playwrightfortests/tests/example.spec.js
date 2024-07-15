d9af79a373ec4938e14066b5867fb2c60dcbe30b
Nestori97 <128605318+Nestori97@users.noreply.github.com>
2024-07-15 12:53:47 +0300

    5.17-5.18

---
+++ kierros5/playwrightfortests/tests/example.spec.js
@@ -0,0 +1,36 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('blogs app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = await page.getByText('Log in to application')
+    await expect(locator).toBeVisible()
+  })
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.getByRole('textbox').last().fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('vaara')
+      await page.getByRole('textbox').last().fill('salasana')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('vaara salasana logged in')).toBeVisible()
+    })
+  })
+})
