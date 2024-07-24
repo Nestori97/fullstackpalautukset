712c4540f2901f68753f77dba910da314d151285
Nestori97 <128605318+Nestori97@users.noreply.github.com>
2024-07-24 10:46:22 +0300

    6.9

--- kierros6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ kierros6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,12 +1,17 @@
 import { voteAnecdote } from '../reducers/anecdoteReducer';
 import { useSelector, useDispatch } from 'react-redux';
 const anecdoteList = () => {
-    const anecdotes = useSelector(state => state);
     const dispatch = useDispatch();
     const vote = (id) => {
         console.log('vote', id);
         dispatch(voteAnecdote(id));
       }
+      const anecdotes = useSelector(state => {
+        if ( state.filter === '' ) {
+          return state.anecdotes
+        }
+        return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+      })
       return(
         <div>
     {anecdotes.map(anecdote =>
