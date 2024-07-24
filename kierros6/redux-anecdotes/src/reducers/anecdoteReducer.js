9f1e22a823398930b246c6c0f19e2765a3d77393
Nestori97 <128605318+Nestori97@users.noreply.github.com>
2024-07-24 12:28:58 +0300

    6.11

--- kierros6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ kierros6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,21 +1,6 @@
 /* eslint-disable no-case-declarations */
+import { createSlice } from '@reduxjs/toolkit'
 
-export const voteAnecdote = (id) => {
-  return {
-    type: 'VOTE',
-    data: { id }
-  }
-}
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: {
-      content,
-      id: getId(),
-      votes: 0
-    }
-  }
-}
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -35,29 +20,30 @@ const asObject = (anecdote) => {
   }
 };
 
-const initialState = anecdotesAtStart.map(asObject);
+
 
 // reducer
-const anecdoteReducer = (state = initialState, action) => {
-  console.log('state now: ', state);
-  console.log('action', action);
-
-  switch (action.type) {
-    case 'NEW_ANECDOTE':
-      return [...state, action.payload]
-    case 'VOTE':
-      const id = action.data.id;
-      const anecdoteToVote = state.find(anecdote => anecdote.id === id);
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState: anecdotesAtStart.map(asObject),
+  reducers: {
+    createAnecdote(state, action) {
+      const newAnecdote = action.payload
+      state.push(newAnecdote)
+    },
+    voteAnecdote(state, action){
+      const id = action.payload
+      const anecdoteToVote = state.find(n => n.id === id)
       const votedAnecdote = {
         ...anecdoteToVote,
         votes: anecdoteToVote.votes + 1
-      };
+      }
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : votedAnecdote
       ).sort((anecdote1,anecdote2) =>anecdote2.votes-anecdote1.votes );
-    default:
-      return state;
+    },
   }
-};
+})
 
-export default anecdoteReducer;
+export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
